61e13f5d28b53640fcbffa935cdc36acd0b72fd4
anastasiyapautarak <153219675+anastasiyapautarak@users.noreply.github.com>
2025-05-14 08:28:32 +0200

    Update ui.R

--- App_5/ui.R
+++ App_5/ui.R
@@ -10,14 +10,17 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  #app title
   titlePanel("Gene Expression View"),
   
   sidebarLayout(
     sidebarPanel(
+      #menu
       selectInput("gene_id", "Select a gene ID:", choices = NULL)
     ),
     
     mainPanel(
+      #TPM expression plot
       plotOutput("expressionPlot")
     )
   )
